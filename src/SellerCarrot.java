a48f40f0b8d3dccb4e5a84da5657f0a565e151bc
Seky30 <martinsekerka1994@gmail.com>
2024-10-02 16:43:50 +0200

    Update SellerCarrot.java

--- src/SellerCarrot.java
+++ src/SellerCarrot.java
@@ -7,4 +7,22 @@ public class SellerCarrot {
     private String cityName;
     private String registrationNumber;
     private Double fuelEconomy;
+    private String name;
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
